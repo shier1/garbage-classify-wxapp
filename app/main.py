dd29c858bede509172118680c6ec5e7071be29d7
shier1 <www.zyanping14@gmail.com>
2022-09-04 17:30:20 +0800

    modify the testapi

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/predict',  methods=['POST'])
 def predict():
-    img_data_base64 = request.form.get(img_data)
+    img_data_base64 = request.form.get('img_data')
     img_data = base64.b64decode(img_data_base64) 
     nparr = np.fromstring(img_data,np.uint8)
     img = cv2.imdecode(nparr,cv2.COLOR_BGR2RGB)
