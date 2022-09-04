df2fcb2a998f3279f5d9a06a1c44c7b45be1dad8
shier1 <www.zyanping14@gmail.com>
2022-09-04 17:17:46 +0800

    test_api

--- app/main.py
+++ app/main.py
@@ -8,20 +8,24 @@ from flask import Flask,Response, jsonify, request
 app = Flask(__name__)
 
 
-
-
-@app.route('/predict',  methods=['post'])
+@app.route('/predict',  methods=['POST'])
 def predict():
-    img_data_base64 = request.POST.get("img_data")
+    img_data_base64 = request.form.get(img_data)
     img_data = base64.b64decode(img_data_base64) 
     nparr = np.fromstring(img_data,np.uint8)
     img = cv2.imdecode(nparr,cv2.COLOR_BGR2RGB)
     res_label = predict_image(img)
     return jsonify({"result":res_label})
 
-# @app.route('/')
-# def index():
-#     return "hello world"
+@app.route('/')
+def index():
+    return "hello world"
+
+@app.route('/test_api', methods=['POST'])
+def test_api():
+    test_name = request.form.get('name')
+    return jsonify({"name":test_name})
+
 
 if __name__ == '__main__':
     allowed_ip_addr = "0.0.0.0"
